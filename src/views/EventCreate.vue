4dc09efb349fecc7727f913d4319739842430a04
linhdongoc <linhhoa.do@gmail.com>
2018-11-24 23:25:50 +0100

    Real World Vuejs

---
+++ src/views/EventCreate.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Event Creating</h1>
+</template>
+
+<script>
+export default {
+  name: 'EventCreate'
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/EventList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
+    <BaseIcon></BaseIcon>
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard'
+import EventService from '@/services/EventService'
+
+export default {
+  name: 'EventList',
+  components: { EventCard },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        /*eslint-disable no-console*/
+        console.log('There was an error:', error.response)
+      })
+  }
+}
+</script>
+
+<style scoped>
+</style>
